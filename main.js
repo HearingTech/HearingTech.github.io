2256e44a29cc868484500dc733f0e77edfb50c29
HearingTech <84751339+HearingTech@users.noreply.github.com>
2023-01-22 18:21:15 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
-// =============== SignUp ===============
+function redireccionar(dir){ location.href="inicio.html";}
+// =============== Mostrar etiquetas ===============
+const loggedOutLinks = document.querySelectorAll('.logged-out'),
+	  loggedInLinks = document.querySelectorAll('.logged-in');
+
+const loginCheck = user => {
+	if (user) {
+		loggedInLinks.forEach(link => link.style.display = 'block');
+		loggedOutLinks.forEach(link => link.style.display = 'none');
+	} else {
+		loggedInLinks.forEach(link => link.style.display = 'none');
+		loggedOutLinks.forEach(link => link.style.display = 'block');
+	}
+};
+
+// =============== Crear cuenta ===============
 const signupForm = document.querySelector('#signup-form'),
 	  sumodal = new bootstrap.Modal('#signupModal');
 
@@ -15,11 +30,11 @@ signupForm.addEventListener('submit',(e) =>{
 			signupForm.reset();
 			//cerrar el modal
 			sumodal.hide();
-			console.log('Se resgistro')
+			console.log('Se resgistro');
 		})
 });
 
-// =============== SingIn ===============
+// =============== Iniciar sesion ===============
 const signinForm = document.querySelector('#login-form'),
 	  simodal = new bootstrap.Modal('#signinModal');
 
@@ -35,10 +50,11 @@ signinForm.addEventListener('submit',(e) =>{
 			signinForm.reset();
 			//cerrar el modal
 			simodal.hide();
-			console.log('Se inicio sesion')
+			console.log('Se inicio sesion');
+			redireccionar();
 		})
 });
-// =============== Log out ===============
+// =============== Cerrar sesion ===============
 const logout = document.querySelector('#logout');
 	logout.addEventListener('click', e => {
 	e.preventDefault();
@@ -75,13 +91,15 @@ const postList = document.querySelector('.posts')
 					<li class="list-group-item list-group-item-action">
 						<h5>${post.title}</h5>
 						<p>${post.description}</p>
+						<button type="button" class="btn btn-light btn-bc-primary" onclick="location.href='inicio.html'"">Mexi Tour</button>
 					</li>
 				`;
 				html += li;
 			});
 			postList.innerHTML = html;
 		} else {
-			postList.innerHTML = '<p class="text-center">Login  to see enter the page</p>'
+			postList.innerHTML = '<p class="text-center fs-2"><span class="text-primary">Inicia sesi&oacuten</span> para entrar a la p&aacutegina</p>'
+								  //<h1 class="p-1 fs-2 border-top border-3
 		}
 	  }
 ;
@@ -92,8 +110,10 @@ auth.onAuthStateChanged(user =>{
 			.get()
 			.then((snapshot) => {
 				setupPost(snapshot.docs);
+				loginCheck(user);
 			})
 	} else {
 		setupPost([]);
+		loginCheck(user);
 	}
 })
